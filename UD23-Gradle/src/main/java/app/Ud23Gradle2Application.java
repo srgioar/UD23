85a2fb7144e0dd08c0c7c7c03e7cf6a993f210b5
Sergio Aragón <saragonr91@gmail.com>
2021-08-11 14:26:38 +0200

    Subida principal

---
+++ UD23-Gradle/src/main/java/app/Ud23Gradle2Application.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ud23Gradle2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ud23Gradle2Application.class, args);
+	}
+
+}
---
+++ UD23-Gradle/src/main/java/app/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+public class HelloWorldController {
+ 
+    @GetMapping
+    public String saludos(@RequestParam(required = false, defaultValue = "") String nombre) {
+        return "".equals(nombre) ? "Hola Mundo!" : "Hola " + nombre;
+    }
+}
